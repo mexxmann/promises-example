a4811f6f83520721cf6dbae8c3cca3637c11246f
Daniel Lishingman <daniel.lishingman@investopedia.com>
2017-07-06 13:59:03 -0600

    removed try/catch as it's redundant after adding error callbacks

--- api/routescb.js
+++ api/routescb.js
@@ -7,55 +7,46 @@ const router = express.Router();
 router.get('/departures/:userEmail/flightDetails/passengerManifest', (req, res) => {
   userEmail = req.params.userEmail;
   util.logWithDate(`[Callback Mode] userEmail: ${userEmail}`);
-  try {
-    // 1. Get user's departure details
-    myModel.getDeparture(userEmail, (departure) => {
+  // 1. Get user's departure details
+  myModel.getDeparture(userEmail, (departure) => {
 
-      // 2. Use departure details to get flight details
-      myModel.getFlightDetails(departure.flightId, (flightDetails) => {
+    // 2. Use departure details to get flight details
+    myModel.getFlightDetails(departure.flightId, (flightDetails) => {
 
-        // 3. Use flight details to get Passenger Manifest
-        myModel.getPassengerManifest(flightDetails.manifestId, (passengerManifest) => {
+      // 3. Use flight details to get Passenger Manifest
+      myModel.getPassengerManifest(flightDetails.manifestId, (passengerManifest) => {
 
-          // Return the result
-          res.status(200).send(passengerManifest);
-        }, (error) => {
-          return res.status(500).send(String(error));
-        });
+        // Return the result
+        res.status(200).send(passengerManifest);
       }, (error) => {
         return res.status(500).send(String(error));
       });
     }, (error) => {
       return res.status(500).send(String(error));
     });
-  } catch (err) {
-    util.logWithDate(`[Callback Mode] Caught <-> exception: ${String(err)}`);
-    return res.status(500).send(String(err));
-  }
+  }, (error) => {
+    return res.status(500).send(String(error));
+  });
 });
 
 router.get('/departures/:userEmail/flightDetails/eta', (req, res) => {
   userEmail = req.params.userEmail;
   util.logWithDate(`[Callback Mode] userEmail: ${userEmail}`);
-  try {
-    // 1. Get user's departure details
-    myModel.getDeparture(userEmail, (departure) => {
+  // 1. Get user's departure details
+  myModel.getDeparture(userEmail, (departure) => {
 
-      // 2. Use departure details to get flight details (note cannot get flight and weather simulaneously)
-      myModel.getFlightDetails(departure.flightId, (flightDetails) => {
+    // 2. Use departure details to get flight details (note cannot get flight and weather simulaneously)
+    myModel.getFlightDetails(departure.flightId, (flightDetails) => {
 
-        // 3. Use departure details to get weather details (note cannot get flight and weather simulaneously)
-        myModel.getForecast(departure.flightTime, (forecast) => {
+      // 3. Use departure details to get weather details (note cannot get flight and weather simulaneously)
+      myModel.getForecast(departure.flightTime, (forecast) => {
 
-          // 3. Use flight details and weather to get ETA
-          myModel.getFlightEta(forecast.weatherOutlook, flightDetails.flightPath, (flightEta) => {
+        // 3. Use flight details and weather to get ETA
+        myModel.getFlightEta(forecast.weatherOutlook, flightDetails.flightPath, (flightEta) => {
 
-            // Return the result
-            util.logWithDate(`[Callback Mode] getFlightEta promise resolved with: ${JSON.stringify(flightEta)}`);
-            return res.status(200).send(flightEta);
-          }, (error) => {
-            return res.status(500).send(String(error));
-          });
+          // Return the result
+          util.logWithDate(`[Callback Mode] getFlightEta promise resolved with: ${JSON.stringify(flightEta)}`);
+          return res.status(200).send(flightEta);
         }, (error) => {
           return res.status(500).send(String(error));
         });
@@ -65,10 +56,9 @@ router.get('/departures/:userEmail/flightDetails/eta', (req, res) => {
     }, (error) => {
       return res.status(500).send(String(error));
     });
-  } catch (err) {
-    util.logWithDate(`[Callback Mode] Caught exception: ${String(err)}`);
-    return res.status(500).send(String(err));
-  }
+  }, (error) => {
+    return res.status(500).send(String(error));
+  });
 });
 
 module.exports = router;
